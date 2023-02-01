c75d9d1934a5586b5760905733d7d37223c82913
Prashant Kumar <thearticle35@gmail.com>
2023-02-01 08:55:38 +0530

    Updates

---
+++ precache-manifest.def6ad954dca65a15deed6f1b0423032.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0dd05bab4054e8fdc3b",
+    "url": "/prashantmishra/static/css/main.065b6b9f.chunk.css"
+  },
+  {
+    "revision": "a0dd05bab4054e8fdc3b",
+    "url": "/prashantmishra/static/js/main.a0dd05ba.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/prashantmishra/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "2589f6afdd50842bede9",
+    "url": "/prashantmishra/static/js/2.2589f6af.chunk.js"
+  },
+  {
+    "revision": "cc9b9c78d3a125da6f72",
+    "url": "/prashantmishra/static/js/runtime~main.cc9b9c78.js"
+  },
+  {
+    "revision": "87a66464853a61a2014a94b2f3ace9de",
+    "url": "/prashantmishra/static/media/prashantresume.87a66464.pdf"
+  },
+  {
+    "revision": "7993fe8faa2b35dc78caaf84ab1fae34",
+    "url": "/prashantmishra/static/media/Prashant.7993fe8f.jpg"
+  },
+  {
+    "revision": "2e2bcb6a7070e99adbcb9bfc975b9642",
+    "url": "/prashantmishra/static/media/PRASHANTKUMAR.2e2bcb6a.pdf"
+  },
+  {
+    "revision": "ae972ed2add7f3a9f5c5a03b68658ea0",
+    "url": "/prashantmishra/index.html"
+  }
+];
